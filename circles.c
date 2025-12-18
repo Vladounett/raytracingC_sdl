944fbb7e9ee4d1d3ed0c30e6fd0fce9e05b27af8
Vladounett <quentin.charlier55@gmail.com>
2025-12-18 23:30:26 +0100

    mid-point algorithm

--- circles.c
+++ circles.c
@@ -5,12 +5,37 @@
 
 void DrawCircle(SDL_Surface* surface, Circle circle){
 
-    for(double x = circle.x - circle.radius; x <= circle.x + circle.radius; x++){
+    //mid-point circle algorithm
+
+    int x = circle.radius;
+    int y = 0;
+    int p = 1 - circle.radius;
+    
+    while(x >= y){
+        SDL_FillRect(surface, &(SDL_Rect){circle.x - x, circle.y + y, 2*x + 1, 1}, 0xFFFFFFFF);
+
+        SDL_FillRect(surface, &(SDL_Rect){circle.x - x, circle.y - y, 2*x + 1, 1}, 0xFFFFFFFF);
+
+        SDL_FillRect(surface, &(SDL_Rect){circle.x - y, circle.y + x, 2*y + 1, 1}, 0xFFFFFFFF);
+
+        SDL_FillRect(surface, &(SDL_Rect){circle.x - y, circle.y - x, 2*y + 1, 1}, 0xFFFFFFFF);
+
+        y++;
+
+        if (p <= 0)
+            p += 2*y + 1;
+        else {
+            x--;
+            p += 2*y - 2*x + 1;
+        }
+    }
+
+    /**for(double x = circle.x - circle.radius; x <= circle.x + circle.radius; x++){
         for(double y = circle.y - circle.radius; y <= circle.y + circle.radius; y++){
 
             if(pow(x - circle.x, 2) + pow(y - circle.y, 2) <= pow(circle.radius, 2)){ //(x−cx)^2+(y−cy)^2 <= r^2
                 SDL_FillRect(surface, &(SDL_Rect){x, y, 1, 1}, 0xFFFFFFFF);
             }
         }
-    }
+    } naive way **/
 }
--- raytracing.c
+++ raytracing.c
@@ -5,15 +5,19 @@
 
 #define WINDOW_WIDTH 1200
 #define WINDOW_HEIGHT 800
+#define MOVE_SPEED 4
 
 int main(){
+
+    const SDL_Rect reset_rect = (SDL_Rect) {0, 0, WINDOW_WIDTH, WINDOW_HEIGHT};
 
     SDL_Init(SDL_INIT_VIDEO);
 
     SDL_Window* mainWindow = SDL_CreateWindow("Raytracing", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, 0);
     SDL_Surface* mainSurface = SDL_GetWindowSurface(mainWindow);
 
-    Circle c = (Circle) {WINDOW_WIDTH/2, WINDOW_HEIGHT/2, 50};
+    Circle ray_circle = (Circle) {WINDOW_WIDTH/2, WINDOW_HEIGHT/2, 40};
+    Circle rayblocking_circle = (Circle) {WINDOW_WIDTH/2 + 300, WINDOW_HEIGHT/2, 100};
 
     short engine_is_running = 1;
     while(engine_is_running){
@@ -21,12 +25,22 @@ int main(){
         while(SDL_PollEvent(&event)){
             if(event.type == SDL_QUIT){
                 engine_is_running = 0;
+            }else if(event.type == SDL_KEYDOWN){
+                switch(event.key.keysym.sym){
+                    case SDLK_UP: ray_circle.y -= MOVE_SPEED; break;
+                    case SDLK_DOWN: ray_circle.y += MOVE_SPEED; break;
+                    case SDLK_LEFT: ray_circle.x -= MOVE_SPEED; break;
+                    case SDLK_RIGHT: ray_circle.x += MOVE_SPEED; break;
+                }
             }
         }
 
-        DrawCircle(mainSurface, c);
+        SDL_FillRect(mainSurface, &reset_rect, 0x00000000); //draw background black before drawing circles
+        DrawCircle(mainSurface, ray_circle);
+        DrawCircle(mainSurface, rayblocking_circle);
+
         SDL_UpdateWindowSurface(mainWindow);
-        SDL_Delay(1000);
+        SDL_Delay(20);
     }
 
     return 0;
